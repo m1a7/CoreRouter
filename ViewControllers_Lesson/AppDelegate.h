8c75cb5c0de2856ed8d8cbfd561c550f0d22b140
M1A1rx <thisismymail03@gmail.com>
2017-07-31 20:51:56 +0200

    Initial Commit

---
+++ ViewControllers_Lesson/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewControllers_Lesson
+//
+//  Created by Uber on 31/07/2017.
+//  Copyright © 2017 Uber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewControllers_Lesson/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewControllers_Lesson
+//
+//  Created by Uber on 31/07/2017.
+//  Copyright © 2017 Uber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
